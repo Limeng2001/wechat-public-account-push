ef0566617ee24ff285c9a9062caee2600ca6e522
Limeng2001 <113318291+Limeng2001@users.noreply.github.com>
2022-09-12 09:50:52 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -20,10 +20,10 @@ export const config = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [“oOwia5rB9bkTix-SAcoCtYjH3O3I”]
+  USERS: [oOwia5rB9bkTix-SAcoCtYjH3O3I]
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "宝宝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "宝宝",
       // 你想对他发送的模板消息的模板ID
